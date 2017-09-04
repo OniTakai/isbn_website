4fa83b8f286f442a1cca0352ad7a909dc327bd71
OniTakai <steven.miller.it@gmail.com>
2017-09-04 18:28:28 -0400

    Just need to get result to final page

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
+require_relative 'isbn_check.rb'
 enable :sessions
 
 get '/' do 
     erb :index
 end 
 
-post '/results' do
- 
+post '/index' do
+session[:my_isbn] = params[:my_isbn].to_s
+session[:isbn_result] = params[:isbn_done].to_s
+session[:isbn_result] = isbn_checker(my_isbn).to_s
+redirect '/results'
 end 
+
+get '/result' do 
+session[:my_isbn] = params[:my_isbn]
+session[:isbn_done] = params[:isbn_done]
+end
